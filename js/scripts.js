06f8085c9133ae89189547ed7873a51d89926f63
Student <example@example.com>
2019-10-18 16:01:06 -0700

    finish css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $(".poke").click(function(event) {
     $(".content").hide();
-    // $(".jumbotron").show();
     $(".filler").show();
   });
+
+$(".hi").click(function(event) {
+
+});
+
 });
